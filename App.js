d8b85b5f7508c1ce4ff28d7e7f0385bce9668a38
ButHetMuc <butle132@gmail.com>
2022-09-18 23:19:01 +0700

    week3

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { Icon,IconComponentProvider } from '@react-native-material/core';
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
-export default function App() {
+import background from "../screen2/materials/background.png";
+
+export default function App() { 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={background} resizeMode = 'cover' style ={styles.ImageBackground} ></ImageBackground>
       <StatusBar style="auto" />
+      <View style = {styles.top}> 
+        <IconComponentProvider IconComponent={MaterialCommunityIcons}>
+          <Icon name='lock' size={100} color = 'black'></Icon>
+        </IconComponentProvider>
+      </View>
+      <View style = {styles.middle}></View >
+      <View style = {styles.bottom}></View>
     </View>
   );
 }
@@ -13,8 +24,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  ImageBackground: {
+    flex:1,
+    justifyContent:'center'
+  }
 });
